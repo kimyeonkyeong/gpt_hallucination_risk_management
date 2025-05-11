d8b7e42942105aec861fe546c323472e4a6460e7
kimyeonkyeong <yk0318@postech.ac.kr>
2025-05-11 21:21:17 +0900

    Update check_similarity.py

--- check_similarity.py
+++ check_similarity.py
@@ -9,3 +9,11 @@ def find_most_similar_laws(question, answer, articles_df, top_k=3):
     hits = util.semantic_search(query_emb, law_embs, top_k=top_k)[0]
     results = [articles_df.iloc[hit['corpus_id']] for hit in hits]
     return results
+
+def find_most_similar_cases(question, answer, case_df, top_k=3):
+    query = question + " " + answer
+    query_emb = model.encode(query, convert_to_tensor=True)
+    case_embs = model.encode(case_df["summary"].tolist(), convert_to_tensor=True)
+    hits = util.semantic_search(query_emb, case_embs, top_k=top_k)[0]
+    results = [case_df.iloc[hit['corpus_id']] for hit in hits]
+    return results
